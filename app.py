7b63e4087eb80348158dc1925e20e72b84dcae2f
Juan Roncancio <109776558+Juicibox@users.noreply.github.com>
2024-03-14 21:50:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+import streamlit as st  
+import numpy as np
+from PIL import Image
+import pytesseract
+
+st.title("Extractor de texto de imágenes")
+
+img_file = st.file_uploader("Cargar imagen", type=['jpg', 'jpeg', 'png', 'webp', 'bmp'])
+if img_file is not None:
+    image = Image.open(img_file)
+    img_array = np.array(image)
+
+text = pytesseract.image_to_string(image)
+st.write(text)
 
