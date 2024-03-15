d970f412822542b5017232921d1f6091521d9af7
Juan Roncancio <109776558+Juicibox@users.noreply.github.com>
2024-03-14 22:04:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ st.title("Extractor de texto de imágenes")
 img_file = st.file_uploader("Cargar imagen", type=['jpg', 'jpeg', 'png', 'webp', 'bmp'])
 if img_file is not None:
     image = Image.open(img_file)
-    img_array = np.array(image)
-    text = pytesseract.image_to_string(image)
-    st.write(text)
+    st.image(image, caption='Imagen cargada', use_column_width=True)  # Mostrar la imagen cargada
 
+    img_array = np.array(image)
+    if img_array is not None:  # Verificar si la imagen se cargó correctamente como un array numpy
+        text = pytesseract.image_to_string(image)
+        st.write("Texto extraído:")
+        st.write(text)
+    else:
+        st.error("Error al cargar la imagen. Por favor, inténtalo de nuevo.")
