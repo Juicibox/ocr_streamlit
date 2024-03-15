08810b0bdfb71179a1475de5c6c370da9146a8bd
Juan Roncancio <109776558+Juicibox@users.noreply.github.com>
2024-03-14 22:19:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,23 +7,16 @@ from paddleocr import PaddleOCR
 st.title("Extractor de texto de imágenes")
 
 img_file = st.file_uploader("Cargar imagen", type=['jpg', 'jpeg', 'png', 'webp', 'bmp'])
-if img_file is not None:
-    image = Image.open(img_file)
-    st.image(image, caption='Imagen cargada', use_column_width=True)  # Mostrar la imagen cargada
-
+if img_file_buffer is not None:
+    image = Image.open(img_file_buffer)
     img_array = np.array(image)
-    if img_array is not None:  # Verificar si la imagen se cargó correctamente como un array numpy
-        text = pytesseract.image_to_string(image)
-        st.write("Texto extraído:")
-        st.write(text)
 
-        ocr_model = PaddleOCR()
-        result = ocr_model.ocr(img_array)
-        result = result[0]
-        texts = [res[1][0] for res in result]
+    # Procesar imagen con PaddleOCR
+    ocr_model = PaddleOCR()
+    result = ocr_model.ocr(img_array)
+    result = result[0]
+    texts = [res[1][0] for res in result]
     
-
+    st.write("Texto extraido")
     # Mostrar resultado
-        st.write(texts)
-    else:
-        st.error("Error al cargar la imagen. Por favor, inténtalo de nuevo.")
+    st.write(texts)
